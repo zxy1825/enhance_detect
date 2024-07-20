cc7a8cdcbbb0c02ed6007539297863c14ea70e52
zxy1825 <619343464@qq.com>
2024-07-20 14:08:11 +0800

    try to push latest code to master

---
+++ models/unet.py
@@ -0,0 +1,43 @@
+'''
+Author: gw00336465 gw00336465@ifyou.com
+Date: 2024-05-09 13:02:17
+LastEditors: gw00336465 gw00336465@ifyou.com
+LastEditTime: 2024-05-22 16:41:09
+FilePath: /UNet/models/unet.py
+Description:unet网络结构实现
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.common import DoubleConv,Up, Down, OutConv
+
+class UNet(nn.Module):
+    def __init__(self, n_channels=3, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, self.n_channels)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
